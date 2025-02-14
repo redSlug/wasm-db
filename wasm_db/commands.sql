f39832d1ce674918d2b71f7e81f37f7c697202d3
bradley <11279144+redSlug@users.noreply.github.com>
2025-02-14 11:55:36 -0500

    init vite for hot reloading

---
+++ wasm_db/commands.sql
@@ -0,0 +1,8 @@
+
+
+CREATE TABLE Persons (id int, name varchar(255));
+dbExecCustom("CREATE TABLE Persons (id int, name varchar(255));")
+
+
+dbExecCustom("DROP TABLE samples;")
+dbExecCustom("SELECT * FROM samples;")
